2749d6a36f049a7ae10f131887937abf8cc16b01
lajanzen <lara-janzen@hotmail.com>
2021-11-25 15:11:06 +0100

    Rename variable

--- src/components/EventCard/EventCard.stories.tsx
+++ src/components/EventCard/EventCard.stories.tsx
@@ -6,7 +6,7 @@ export default {
   component: EventCard,
 };
 
-export const Flohmarkt = (): JSX.Element => (
+export const Fleamarket = (): JSX.Element => (
   <EventCard
     imgSrc="https://mobil.ksta.de/image/25684552/2x1/600/300/f6b5575094f8698991e7455e5a22460b/my/flohmarkt-symbolimagolem50254643h.jpg"
     name="Flohmarkt am Heumarkt"
