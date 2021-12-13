230fed6e560795ace0c7a26810ac23da50d6121e
lajanzen <lara-janzen@hotmail.com>
2021-12-13 16:02:56 +0100

    Refactor EventCard

--- src/components/EventCard/EventCard.stories.tsx
+++ src/components/EventCard/EventCard.stories.tsx
@@ -6,14 +6,53 @@ export default {
   component: EventCard,
 };
 
+const exampleEventLiked = {
+  image:
+    "https://mobil.ksta.de/image/25684552/2x1/600/300/f6b5575094f8698991e7455e5a22460b/my/flohmarkt-symbolimagolem50254643h.jpg",
+  title: "Flohmarkt am Heumarkt",
+  startDate: "2021-12-12",
+  time: "11 bis 16 Uhr",
+  location: "Heumarkt",
+  fee: "Kostenlos",
+  link: "example",
+  endDate: "example",
+  description: "example",
+  zip: 123,
+  city: "example",
+  district: "example",
+  area: "example",
+  street: "example",
+  streetNumber: 123,
+  transportation: "example",
+  latitude: 123,
+  longitude: 123,
+};
+
+const exampleEventNotLiked = {
+  image:
+    "https://mobil.ksta.de/image/25684552/2x1/600/300/f6b5575094f8698991e7455e5a22460b/my/flohmarkt-symbolimagolem50254643h.jpg",
+  title: "Flohmarkt am Heumarkt",
+  startDate: "2021-12-12",
+  time: "11 bis 16 Uhr",
+  location: "Heumarkt",
+  fee: "Kostenlos",
+  link: "example",
+  endDate: "example",
+  description: "example",
+  zip: 123,
+  city: "example",
+  district: "example",
+  area: "example",
+  street: "example",
+  streetNumber: 123,
+  transportation: "example",
+  latitude: 123,
+  longitude: 123,
+};
+
 export const Liked = (): JSX.Element => (
   <EventCard
-    imgSrc="https://mobil.ksta.de/image/25684552/2x1/600/300/f6b5575094f8698991e7455e5a22460b/my/flohmarkt-symbolimagolem50254643h.jpg"
-    name="Flohmarkt am Heumarkt"
-    date="2021-12-12"
-    time="11 bis 16 Uhr"
-    location="Heumarkt"
-    price="Kostenlos"
+    content={exampleEventLiked}
     liked={true}
     onClick={() => console.log("liked")}
   />
@@ -21,12 +60,7 @@ export const Liked = (): JSX.Element => (
 
 export const NotLiked = (): JSX.Element => (
   <EventCard
-    imgSrc="https://mobil.ksta.de/image/25684552/2x1/600/300/f6b5575094f8698991e7455e5a22460b/my/flohmarkt-symbolimagolem50254643h.jpg"
-    name="Flohmarkt am Heumarkt"
-    date="2021-12-12"
-    time="11 bis 16 Uhr"
-    location="Heumarkt"
-    price="Kostenlos"
+    content={exampleEventNotLiked}
     liked={false}
     onClick={() => console.log("liked")}
   />
--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,30 +1,23 @@
 import React from "react";
+import { Event } from "../../types";
 import HeartIconEmpty from "../Icons/HeartIconEmpty";
 import HeartIconFilled from "../Icons/HeartIconFilled";
 import styles from "./EventCard.module.css";
 
 type EventCardProps = {
-  imgSrc: string;
-  name: string;
-  date: string;
-  time: string;
-  location: string;
-  price: string;
-  liked?: boolean;
+  content: Event;
+  liked: boolean;
   onClick?: () => void;
 };
 
 export default function EventCard({
-  imgSrc,
-  name,
-  date,
-  time,
-  location,
-  price,
+  content,
   liked,
   onClick,
 }: EventCardProps): JSX.Element {
-  const dateObject = new Date(date);
+  const { image, title, startDate, time, location, fee } = content;
+
+  const dateObject = new Date(startDate);
   const options: Intl.DateTimeFormatOptions = {
     weekday: "long",
     year: "numeric",
@@ -35,12 +28,20 @@ export default function EventCard({
 
   return (
     <article className={styles.container}>
-      <img className={styles.img} src={imgSrc} alt="" />
-      <h2 className={styles.eventName}>{name}</h2>
+      <img
+        className={styles.img}
+        src={
+          image == null
+            ? "https://images.unsplash.com/photo-1543145480-b8d11d3581a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=929&q=80"
+            : image
+        }
+        alt=""
+      />
+      <h2 className={styles.eventName}>{title}</h2>
       <p>{eventDate}</p>
       <p>{time}</p>
       <p>{location}</p>
-      <p>{price}</p>
+      <p>{fee}</p>
       <button className={styles.button} onClick={onClick}>
         {liked ? <HeartIconFilled /> : <HeartIconEmpty />}
       </button>
