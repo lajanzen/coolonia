5146c946d3e4b6ad83bfb4f1275fcaff7acb9d66
lajanzen <lara-janzen@hotmail.com>
2021-12-22 16:21:39 +0100

    Create filter group

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,9 +1,40 @@
 import React, { useEffect, useState } from "react";
 import EventCard from "../../components/EventCard/EventCard";
 import LogoSmall from "../../components/Icons/LogoSmall";
+import Tag from "../../components/Tag/Tag";
 import { Event } from "../../types";
-
 import styles from "./Events.module.css";
+
+const days = [
+  {
+    name: "heute",
+    value: "today",
+  },
+  {
+    name: "Di",
+    value: "tuesday",
+  },
+  {
+    name: "Mi",
+    value: "wednesday",
+  },
+  {
+    name: "Do",
+    value: "thursday",
+  },
+  {
+    name: "Fr",
+    value: "friday",
+  },
+  {
+    name: "Sa",
+    value: "saturday",
+  },
+  {
+    name: "So",
+    value: "sunday",
+  },
+];
 
 export default function Events() {
   const [isLiked, setIsLiked] = useState(false);
@@ -18,6 +49,16 @@ export default function Events() {
   return (
     <div className={styles.container}>
       <div className={styles.events}>
+        <div className={styles.tags}>
+          {days.map((day) => (
+            <Tag
+              tagName={day.name}
+              value={day.value}
+              name="days"
+              key={day.name}
+            />
+          ))}
+        </div>
         {events.map((event: Event) => (
           <EventCard
             content={event}
